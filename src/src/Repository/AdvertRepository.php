f118998572be773d3663de3a015b915735993190
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-07 15:59:58 +0200

    Add advert Repo

---
+++ src/src/Repository/AdvertRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Advert;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Advert>
+ *
+ * @method Advert|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Advert|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Advert[]    findAll()
+ * @method Advert[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AdvertRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Advert::class);
+    }
+
+    public function save(Advert $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Advert $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
