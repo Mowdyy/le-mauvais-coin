1950e09f09b4edcdf4a5a5fe7e16d63d07ca1ba9
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-24 17:04:16 +0200

    add feature find advert by id

--- src/src/Controller/AdvertController.php
+++ src/src/Controller/AdvertController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Advert;
+use App\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdvertController extends AbstractController
 {
-    #[Route('/advert', name: 'app_advert')]
-    public function index(): Response
+    #[Route('/advert/{id}', name: 'app_advert')]
+    public function index($id, AdvertRepository $AdvertRepository): Response
     {
-        return $this->render('advert/index.html.twig', [
-            'controller_name' => 'AdvertController',
-        ]);
+        $advert = $AdvertRepository->findOneById($id);
+        if (!$advert) {
+            throw $this->createNotFoundException("L'annonce que vous recherchez n'existe pas, entrez un ID valide :)");
+        } else {
+            return $this->render('advert/index.html.twig', [
+                'advert' => $advert,
+            ]);
+        }
     }
 }
