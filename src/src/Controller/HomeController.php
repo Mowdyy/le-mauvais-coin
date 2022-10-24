b2d0dedde48e4ae17bd5ecad75d7da7f7475e74a
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-24 17:34:25 +0200

    Adverts finies mais pas stylisées

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AdvertRepository $advertRepository)
     {
+        $adverts = $advertRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'adverts' => $adverts,
         ]);
     }
 }
