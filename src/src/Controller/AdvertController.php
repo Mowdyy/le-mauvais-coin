ca23ee738b645013aa2e8189a23224aa9e441ee3
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-24 17:54:20 +0200

    Add reRoute if no id param

--- src/src/Controller/AdvertController.php
+++ src/src/Controller/AdvertController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Advert;
 use App\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdvertController extends AbstractController
 {
     #[Route('/advert/{id}', name: 'app_advert')]
-    public function index($id, AdvertRepository $AdvertRepository): Response
+    public function findAdvertById($id, AdvertRepository $AdvertRepository): Response
     {
         $advert = $AdvertRepository->findOneById($id);
         if (!$advert) {
-            throw $this->createNotFoundException("L'annonce que vous recherchez n'existe pas, entrez un ID valide :)");
+            throw $this->createNotFoundException("L'annonce que vous recherchez n'existe pas :'(");
         } else {
             return $this->render('advert/index.html.twig', [
                 'advert' => $advert,
             ]);
         }
+    }
+
+    #[Route('/advert')]
+    public function redirectToHome()
+    {
+        return $this->redirectToRoute('app_home');
     }
 }
