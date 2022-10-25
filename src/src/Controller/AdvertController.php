9e797908af1711116da2f20c0e63fe6a0172dad9
louis fortrie <louis.fortrie4@gmail.com>
2022-10-25 16:19:04 +0200

    first commit

--- src/src/Controller/AdvertController.php
+++ src/src/Controller/AdvertController.php
@@ -10,6 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 
 class AdvertController extends AbstractController
 {
@@ -21,14 +25,47 @@ class AdvertController extends AbstractController
             'adverts' => $adverts,
         ]);
     }
+<<<<<<< Updated upstream
 
     #[Route('/advert/add', name: 'app_advert_add', methods: ['GET', 'POST'])]
+=======
+    
+    #[Route('/advert', name: 'app_add_advert', methods: ['GET', 'POST'])]
+>>>>>>> Stashed changes
     public function addAdvert(Request $request, EntityManagerInterface $entityManagerInterface): Response
     {
         $advert = new Advert();
         $form = $this->createForm(AdvertType::class, $advert);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $product = new Product();
+            $form = $this->createForm(ProductType::class, $product);
+            $form->handleRequest($request);
+            $image = $form->get('image')->getData();
+            
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+                
+                // Move the file to the directory where brochures are stored
+                try {
+                    $image->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $advert->setImageFileName($newFilename);
+            }
+            
+            // ... persist the $product variable or any other work
+            
+            return $this->redirectToRoute('app_home');
             $advert = $form->getData();
             $entityManagerInterface->persist($advert);
             $entityManagerInterface->flush();
@@ -38,6 +75,7 @@ class AdvertController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+<<<<<<< Updated upstream
 
     #[Route('/advert/{id}', name: 'app_advert_page')]
     public function findAdvertById($id, AdvertRepository $advertRepository): Response
@@ -53,6 +91,10 @@ class AdvertController extends AbstractController
     }
 
     #[Route('/advert/{id}/delete', name: 'app_advert_delete', methods: ['GET', 'POST'])]
+=======
+    
+    #[Route('/advert/{id}/delete', name: 'app_delete_advert', methods: ['GET', 'POST'])]
+>>>>>>> Stashed changes
     public function deleteAdvert($id, AdvertRepository $advertRepository) 
     {
         $advert = $advertRepository->findOneById($id);
