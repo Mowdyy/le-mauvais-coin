e445807f8f4ae9bbfd36328222d5574aa2eef64e
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-25 11:24:48 +0200

    Revert "User"
    This reverts commit f10144f0bbcb62c22a0942a41ccdfedf9a8c1a08.

--- src/src/Controller/QuestionController.php
+++ src/src/Controller/QuestionController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+<<<<<<< HEAD:src/src/Controller/QuestionController.php
 class QuestionController extends AbstractController
 {
     #[Route('/question', name: 'app_question')]
@@ -13,6 +14,15 @@ class QuestionController extends AbstractController
     {
         return $this->render('question/index.html.twig', [
             'controller_name' => 'QuestionController',
+=======
+class LogController extends AbstractController
+{
+    #[Route('/log', name: 'app_log')]
+    public function index(): Response
+    {
+        return $this->render('log/index.html.twig', [
+            'controller_name' => 'LogController',
+>>>>>>> parent of f10144f (User):src/src/Controller/UserController.php
         ]);
     }
 }
