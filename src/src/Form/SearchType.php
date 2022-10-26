804622ecf63d0e9197c9e92bd0f43b8f423f753c
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-26 10:28:50 +0200

    SearchType

--- src/src/Form/SearchType.php
+++ src/src/Form/SearchType.php
@@ -3,16 +3,16 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SearchType extends AbstractType
+class SearchFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Rechercher')
-            ->add('Filtrer', SubmitType::class, [ 'label'=>'Filtrer' ]);
+            ->add('search', SearchType::class)
         ;
     }
 
