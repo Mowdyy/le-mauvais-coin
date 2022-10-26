7665327b06cadf3ea15cc5500ac64eb90257fa1f
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-26 11:03:04 +0200

    fix searchtype

--- src/src/Form/SearchType.php
+++ src/src/Form/SearchType.php
@@ -3,11 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SearchFormType extends AbstractType
+class SearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
