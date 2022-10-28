fe760fc3c9c4d132ae73c6ecc3a6ffc666eba9a4
Benoît Favrie <benoit14.favrie@gmail.com>
2022-10-28 16:21:05 +0200

    Votes

--- src/src/Controller/VoteController.php
+++ src/src/Controller/VoteController.php
@@ -11,25 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class VoteController extends AbstractController
 {
     #[Route('/vote-up/{id}', name: 'app_vote_up')]
-    public function voteUp(Request $request, UserRegister $user, EntityManagerInterface $entityManagerInterface)
+    public function voteUp($id, Request $request, UserRegister $user, EntityManagerInterface $entityManagerInterface)
     {
         $vote = $user->getVotes();
         $user->upVote($vote);
-        $entityManagerInterface->persist($user);
+
         $entityManagerInterface->flush();
-        
+
         $route = $request->headers->get('referer');
         return $this->redirect($route);
     }
 
     #[Route('/vote-down/{id}', name: 'app_vote_down')]
-    public function voteDown(Request $request, UserRegister $user, EntityManagerInterface $entityManagerInterface)
+    public function voteDown($id, Request $request, UserRegister $user, EntityManagerInterface $entityManagerInterface)
     {
         $vote = $user->getVotes();
         $user->downVote($vote);
-        $entityManagerInterface->persist($user);
-        $entityManagerInterface->flush();
 
+        $entityManagerInterface->flush();
+        
         $route = $request->headers->get('referer');
         return $this->redirect($route);
     }
