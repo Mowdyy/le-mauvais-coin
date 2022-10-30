4c6219526d902495067bdfdfac292453275052df
unknown <mkacem6@outlook.com>
2022-10-30 22:21:10 +0100

    form that will allow us to complete our research

--- src/src/Form/TagFormType.php
+++ src/src/Form/TagFormType.php
@@ -9,20 +9,56 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TagFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('designation')
-            ->add('adverts')
-            ->add('tag')
+            ->add('q', TextType::class, [
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Rechercher'
+                ]
+            ])
+            ->add('categories', EntityType::class, [
+                'label' => false,
+                'required' => false,
+                'class' => Category::class,
+                'expanded' => true,
+                'multiple' => true
+            ])
+            ->add('min', NumberType::class, [
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Prix min'
+                ]
+            ])
+            ->add('max', NumberType::class, [
+                'label' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Prix max'
+                ]
+            ])
+            ->add('promo', CheckboxType::class, [
+                'label' => 'En promotion',
+                'required' => false,
+            ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Tag::class,
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+            'csrf_protection' => false
         ]);
     }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+
 }
