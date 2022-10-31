22440ef1b2f1a2aa938db763c19c6a00a4566eb8
Louis Fortrie <61594466+louisFortrie@users.noreply.github.com>
2022-10-31 16:35:10 +0100

    searchbar working an all pages (#20)

--- src/src/Controller/SearchController.php
+++ src/src/Controller/SearchController.php
@@ -6,17 +6,40 @@ use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\AdvertRepository;
+
+
+
+use App\Form\SearchAdvertType;
+
 
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-    public function index(): Response
+    public function index(Request $request,AdvertRepository $advertRepository): Response
     {
-        $searchBar = $this->createForm(SearchType::class);
-
+        $searchBar = $this->createForm(SearchAdvertType::class, null,[
+            'action' => $this->generateUrl('app_handleSearch'),
+        ]);
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
             'searchForm' => $searchBar->createView()
         ]);
+        
     }
+
+    #[Route('/handleSearch', name: 'app_handleSearch', methods: ['GET','POST'])]
+    public function handleSearch(Request $request, AdvertRepository $advertRepository) {
+        //dd($request->request->all('search_advert'));
+        $query = $request->request->all('search_advert')['keyword'];
+        if($query){
+            $adverts = $advertRepository->getFilteredAdverts($query);
+        }else{
+            $adverts = $advertRepository->findAll();
+        }
+        return $this->render('home/index.html.twig', [
+            'adverts' => $adverts,
+            // 'filteredAdverts' => $advert
+        ]);
+    }
+
 }
